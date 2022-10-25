311159f8ae149591e88b223a200892f8f8f91a18
Ryan Pyles <37378575+ryanpyles@users.noreply.github.com>
2022-10-24 23:44:05 -0400

    completed assignment

---
+++ main.js
@@ -0,0 +1,24 @@
+let paragraph = document.querySelector('p');
+paragraph.style.color = "lightblue";
+
+let head = document.querySelector('h1');
+head.style.fontSize = "48px";
+
+paragraph.innerText = " Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress."
+
+let item13 = document.querySelector('#item-13');
+item13.style.opacity = "50%";
+
+let item3 = document.querySelector("#item-3");
+item3.innerText = "I say, 'Hi!'"
+
+let img = document.querySelector("img");
+img.src = "https://envato-shoebox-0.imgix.net/affe/ba06-c02c-4207-86ba-d64a2bbd9d25/lkunl_180917_014.jpg?auto=compress%2Cformat&fit=max&mark=https%3A%2F%2Felements-assets.envato.com%2Fstatic%2Fwatermark2.png&markalign=center%2Cmiddle&markalpha=18&w=1000&s=3ef67a79e723dfa3c97ff3b382e93cde";
+img.style.height = "300px";
+
+let img2 = document.querySelector('#img2');
+
+img2.style.height = "300px";
+
+let li16 = document.querySelector('#item-16');
+li16.innerText = "Won't get fooled again";
